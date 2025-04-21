af3ea49e4cdcdec82f83b2801c35234c8af35bbe
anguyen0501 <20h1120185@ut.edu.vn>
2025-04-21 21:47:08 +0700

    feat:building contact form component, create reveal on scroll

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,4 @@
+import { RevealOnScroll } from "../RevealOnScroll";
 import {
   SiReact,
   SiNextdotjs,
@@ -30,99 +31,101 @@ const About = () => {
       id="about"
       className="min-h-screen flex flex-col items-center justify-center py-20"
     >
-      <div className="max-w-3xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 text-center bg-clip-text text-transparent">
-          {" "}
-          About Me
-        </h2>
-        <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-          <p className="text-grey-300 mb-6">
-            I am a 4th-year student with 3 months of experience in ReactJs
-            technologies. I have cultivated a passion for learning new
-            technologies and delving into documentation. I love teamwork and
-            tend to work well within a group. I am always eager to learn and
-            grow in.
-          </p>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="rounded-lg p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-700 text-center bg-clip-text text-transparent">
-                Frontend
-              </h3>
-              <div className="flex flex-wrap gap-2">
-                {frontendSkills.map(({ name, icon: Icon }) => (
-                  <span
-                    key={name}
-                    className="flex items-center bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm font-semibold hover:bg-blue-500/20 transition"
-                  >
-                    <Icon className="mr-2 w-4 h-4" />
-                    {name}
-                  </span>
-                ))}
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 text-center bg-clip-text text-transparent">
+            {" "}
+            About Me
+          </h2>
+          <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-grey-300 mb-6">
+              I am a 4th-year student with 3 months of experience in ReactJs
+              technologies. I have cultivated a passion for learning new
+              technologies and delving into documentation. I love teamwork and
+              tend to work well within a group. I am always eager to learn and
+              grow in.
+            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-lg p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-700 text-center bg-clip-text text-transparent">
+                  Frontend
+                </h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map(({ name, icon: Icon }) => (
+                    <span
+                      key={name}
+                      className="flex items-center bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm font-semibold hover:bg-blue-500/20 transition"
+                    >
+                      <Icon className="mr-2 w-4 h-4" />
+                      {name}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="rounded-lg p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-700 text-center bg-clip-text text-transparent">
+                  Backend
+                </h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map(({ name, icon: Icon }) => (
+                    <span
+                      key={name}
+                      className="flex items-center bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm font-semibold hover:bg-blue-500/20 transition"
+                    >
+                      <Icon className="mr-2 w-4 h-4" />
+                      {name}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
-            <div className="rounded-lg p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-700 text-center bg-clip-text text-transparent">
-                Backend
-              </h3>
-              <div className="flex flex-wrap gap-2">
-                {backendSkills.map(({ name, icon: Icon }) => (
-                  <span
-                    key={name}
-                    className="flex items-center bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm font-semibold hover:bg-blue-500/20 transition"
-                  >
-                    <Icon className="mr-2 w-4 h-4" />
-                    {name}
-                  </span>
-                ))}
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-lg border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education</h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong>Studying Web Developer</strong> - University of
+                  Transport Ho Chi Minh City (2020 - 2024)
+                </li>
+                <li>
+                  Degree grade: <strong>Good</strong> - GPA 3.2/4.0
+                </li>
+                <li>
+                  <strong>Toeic</strong> - 600/990 (2022)
+                </li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-lg border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 💼 Work Experience</h3>
+              <div className="space-y-4 text-grey-300">
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Web Development Intern at ALTA Software (12/2024 - 02/2025)
+                  </h4>
+                  <p className="text-sm text-gray-400">
+                    - Developed and maintained web applications using React and
+                    Node.js.
+                  </p>
+                  <p className="text-sm text-gray-400">
+                    - Assisted in building front-end components and integrating
+                    RESTful APIs.
+                  </p>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Web Development Freelancer (02/2025 - Present)
+                  </h4>
+                  <p className="text-sm text-gray-400">
+                    - I have worked on several freelance projects, including:
+                    Wordpress website development, ReactJs website development.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-          <div className="p-6 rounded-lg border-white/10 border hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4"> 🏫 Education</h3>
-            <ul className="list-disc list-inside text-gray-300 space-y-2">
-              <li>
-                <strong>Studying Web Developer</strong> - University of
-                Transport Ho Chi Minh City (2020 - 2024)
-              </li>
-              <li>
-                Degree grade: <strong>Good</strong> - GPA 3.2/4.0
-              </li>
-              <li>
-                <strong>Toeic</strong> - 600/990 (2022)
-              </li>
-            </ul>
-          </div>
-          <div className="p-6 rounded-lg border-white/10 border hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4"> 💼 Work Experience</h3>
-            <div className="space-y-4 text-grey-300">
-              <div>
-                <h4 className="font-semibold">
-                  {" "}
-                  Web Development Intern at ALTA Software (12/2024 - 02/2025)
-                </h4>
-                <p className="text-sm text-gray-400">
-                  - Developed and maintained web applications using React and
-                  Node.js.
-                </p>
-                <p className="text-sm text-gray-400">
-                  - Assisted in building front-end components and integrating
-                  RESTful APIs.
-                </p>
-                <h4 className="font-semibold">
-                  {" "}
-                  Web Development Freelancer (02/2025 - Present)
-                </h4>
-                <p className="text-sm text-gray-400">
-                  - I have worked on several freelance projects, including:
-                  Wordpress website development, ReactJs website development.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      </RevealOnScroll>
     </section>
   );
 };
--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
-
+import { RevealOnScroll } from "../RevealOnScroll";
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section
+      id="contact"
+      className="min-h-screen flex flex-col items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="px-4 w-150">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            Get in touch
+          </h2>
+          <form action="" className="space-y-6">
+            <div className="relative">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                required
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Your Name"
+              />
+            </div>
+            <div className="relative">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div className="relative">
+              <textarea
+                name="message"
+                id="message"
+                rows={5}
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Write your message here..."
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+            >
+              Send Message
+            </button> 
+          </form>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
